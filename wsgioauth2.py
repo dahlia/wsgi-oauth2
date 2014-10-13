8262f110298092ec20d4e594be359546d9c9202a
Hong Minhee <minhee@dahlia.kr>
2014-10-13 18:11:34 +0900

    Fix bug that ignores access_token when url has qs

--- wsgioauth2.py
+++ wsgioauth2.py
@@ -372,7 +372,7 @@ class AccessToken(dict):
         :type headers: :class:`collections.Mapping`
 
         """
-        url += '&' if '?' in url else '?' + 'access_token=' + self.access_token
+        url += ('&' if '?' in url else '?') + 'access_token=' + self.access_token
         request = urllib2.Request(url, headers=headers)
         return urllib2.urlopen(request)
 
